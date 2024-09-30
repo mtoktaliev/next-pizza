c5243fe8477bb1b6c0509cc0a7c8624ccf336b53
maksat <mtoktalievmegacom@gmail.com>
2024-09-30 21:37:32 +0600

    first commit

---
+++ components/ui/index.ts
@@ -0,0 +1,10 @@
+export { Button } from "./button";
+export { Checkbox } from "./checkbox";
+export { Dialog } from "./dialog";
+export { Drawer } from "./drawer";
+export { Input } from "./input";
+export { Popover } from "./popover";
+export { Select } from "./select";
+export { Slider } from "./slider";
+export { RadioGroup } from "./radio-group";
+export { RangeSlider } from "./range-slider";
